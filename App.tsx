22b4068844fcbbb85c40d90286f753144e39a7e3
Thais Helena Moretto <CIANDT\thaism@lnb028293cps.ciandt.global>
2023-08-10 21:33:47 -0300

    Cria projeto e realiza configuracoes iniciais

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Nunito_400Regular,
+  Nunito_700Bold,
+} from "@expo-google-fonts/nunito";
+import theme from "@theme/index";
+import { Loading } from "@components/Loading";
 
 export default function App() {
+  const [fonstLoaded] = useFonts({ Nunito_400Regular, Nunito_700Bold });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <StatusBar style="light" backgroundColor="transparent" translucent />
+      {fonstLoaded ? <Loading /> : <Loading />}
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
